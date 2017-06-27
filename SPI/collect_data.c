05d9d756c3bc7592fc3d248efcf0ef213aaea19e
Steven <steven0peters@gmail.com>
2017-06-27 22:58:45 +0000

    some results and speedtests done

--- SPI/collect_data.c
+++ SPI/collect_data.c
@@ -3,7 +3,10 @@
  * 
  * 	comments: wiringPiSPISetup(0, 25000000 ) -> 25 MHz asked, 21.3 MHz measured with scope
  * 											 -> 50 MHz asked, 42.6 MHz measured with scope
- * 	to do: -write result to file 
+ * 
+ * 			  a subdirectory "results/" is assumed
+ * 	
+ * 	to do: fprintf multiple samples at a time to improve performance + test speed
  * 
  * 
  * 
@@ -17,6 +20,8 @@
 #include <errno.h>
 #include <string.h>
 #include <stdint.h>
+#include <time.h>
+#include <unistd.h>
 
 /* print byte values in binary */
 #define BYTE_TO_BINARY(byte) \
@@ -44,7 +49,10 @@ int main(void)
 	pthread_t thread_heartbeat;
 	unsigned char SPI_buffer[4] = {0}; /* 1 char = 8 bit -> 32 bit buffer */
 	uint32_t index = 0;
-
+	FILE * fp;
+	char filename[50], dir[20]; /* string containing filename */
+	time_t now;
+	
 	wiringPiSetup();
 	
 	/* configure pins */
@@ -64,7 +72,19 @@ int main(void)
 	{
 		fprintf(stderr, "Error creating thread\n");
 		return 1;
+	}
+	
+	/* open new file, with timestamp, to write samples to */
+	now = time(0)+7200; /* add 2 hours = 7200 sec for GMT+02 */
+	strftime( filename , 50 , "%d-%m-%y_%H:%M:%S" , localtime(&(now)));
+	
+	strcpy(dir,"results/"); /* write in directory "results/" */
+	fp = fopen( strcat(dir,filename) ,"a");
+	if (fp == NULL )
+	{
+		printf("Error opening file: %s\n", strerror(errno));
 	}	
+	
 	
 	while(1)
 	{
@@ -73,25 +93,32 @@ int main(void)
 		/* if PIC signals RBP */
 		if (digitalRead(3) == HIGH )
 		{
+			//usleep(15.625); //to test: frequency of 64000 Hz
+			
 			/* collect sample; 32 bits */
 			digitalWrite(2, HIGH); /* debugging led */
 			
 			wiringPiSPIDataRW(0, SPI_buffer , 4 ); /* channel 0; 4 byte -> 32 bit */ 
 
+			/* write samples to text file */
+//			fprintf(fp,"%d\t",index); 
+			fprintf(fp,"%02X%02X%02X%02X\n",*SPI_buffer,*(SPI_buffer+1),*(SPI_buffer+2),*(SPI_buffer+3) ); 
+						
 			
 			/* print received SPI word (32 bit) */
 			/* ///print index */
-			printf("%d\t",index); 
+//			printf("%d\t",index); 
 			
 			/* ///print SPI word in hexadecimal */
-			printf("%X%X%X%X\t\t",*SPI_buffer,*(SPI_buffer+1),*(SPI_buffer+2),*(SPI_buffer+3) ); 
+//			printf("%02X%02X%02X%02X\t\t",*SPI_buffer,*(SPI_buffer+1),*(SPI_buffer+2),*(SPI_buffer+3) ); 
 
 			/* ///print binary representation */
-			printf(LONG_PRINT_PATTERN,BYTE_TO_BINARY(*SPI_buffer),BYTE_TO_BINARY(* (SPI_buffer+1) ), \
+//			printf(LONG_PRINT_PATTERN,BYTE_TO_BINARY(*SPI_buffer),BYTE_TO_BINARY(* (SPI_buffer+1) ), \
 				BYTE_TO_BINARY(* (SPI_buffer+2) ),BYTE_TO_BINARY(* (SPI_buffer+3) ) );
 			
+			
+			
 			index++;
-			delay(1);
 		}
 		else
 		{
